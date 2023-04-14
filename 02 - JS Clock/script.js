5d43faf3c2b6f937dfaaa9758cd4929236dc56a1
SabrinaBarros <sabrina.barros2045@gmail.com>
2023-04-14 00:15:51 -0300

    fix(js-clock): reset glitch

--- 02 - JS Clock/script.js
+++ 02 - JS Clock/script.js
@@ -1,6 +1,7 @@
+const hoursPointer = document.querySelector('.hours');
 const secondsPointer = document.querySelector('.seconds');
 const minutesPointer = document.querySelector('.minutes');
-const hoursPointer = document.querySelector('.hours');
+const allPointers = document.querySelectorAll('.pointer');
 
 const setDate = () => {
   const currentTime =  new Date();
@@ -16,6 +17,12 @@ const setDate = () => {
   const seconds = currentTime.getSeconds();
   const secondsDegrees = ((seconds / 60) * 360) + 90;
   secondsPointer.style.transform = `rotate(${secondsDegrees}deg)`;
-}
+
+  if (secondsDegrees === 90) {
+    allPointers.forEach(pointer => pointer.style.transition = 'none');
+  } else {
+    allPointers.forEach(pointer => pointer.style.transition = '');
+  };
+};
 
 setInterval(setDate, 1000);
